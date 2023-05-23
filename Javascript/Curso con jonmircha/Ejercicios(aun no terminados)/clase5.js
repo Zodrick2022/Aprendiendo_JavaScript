e425fe25e49386f611a5fbfd67c382e3015d4b02
Eduard Paredes <106275524+Zodrick2022@users.noreply.github.com>
2023-05-23 15:58:21 -0500

    Add files via upload

---
+++ Javascript/Curso con jonmircha/Ejercicios(aun no terminados)/clase5.js
@@ -0,0 +1,5 @@
+/*
+15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
+16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
+17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+*/
---
+++ Javascript/Curso con jonmircha/Ejercicios(aun no terminados)/clase6.js
@@ -0,0 +1,6 @@
+/*
+18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+
+*/
