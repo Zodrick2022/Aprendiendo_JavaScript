86483bccef96750a940bd22b34c503cb0ee666cb
Eduard Paredes <106275524+Zodrick2022@users.noreply.github.com>
2023-05-31 16:01:24 -0500

    Add files via upload

---
+++ Curso con jonmircha/Ejercicios(aun no terminados)/clase4.js
@@ -0,0 +1,7 @@
+/*
+12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+
+En el video 38 te daré mi solución, antes de verlo trata de resolver los ejercicios, y comparte tus resultados en alguna plataforma como GitHub o CodePen y comparte el enlace de tus soluciones en los comentarios de este video. 
+*/
